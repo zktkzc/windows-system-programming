f392332ba58e4b77dcecd01f1cd4d8b28e788668
tkzc <2238983030@qq.com>
2023-01-22 17:49:01 +0800

    简易修改器

---
+++ 简易修改器/MFCLibrary1/CSMFCDLL.cpp
@@ -0,0 +1,57 @@
+﻿// CSMFCDLL.cpp: 实现文件
+//
+
+#include "pch.h"
+#include "MFCLibrary1.h"
+#include "afxdialogex.h"
+#include "CSMFCDLL.h"
+
+
+// CCSMFCDLL 对话框
+
+IMPLEMENT_DYNAMIC(CCSMFCDLL, CDialogEx)
+
+CCSMFCDLL::CCSMFCDLL(CWnd* pParent /*=nullptr*/)
+	: CDialogEx(IDD_DIALOG1, pParent)
+	, m_Value(0)
+{
+
+}
+
+CCSMFCDLL::~CCSMFCDLL()
+{
+}
+
+void CCSMFCDLL::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+	DDX_Text(pDX, IDC_EDIT1, m_Value);
+}
+
+
+BEGIN_MESSAGE_MAP(CCSMFCDLL, CDialogEx)
+	ON_BN_CLICKED(IDC_BUTTON1, &CCSMFCDLL::OnBnClickedButton1)
+END_MESSAGE_MAP()
+
+
+// CCSMFCDLL 消息处理程序
+
+
+void CCSMFCDLL::OnBnClickedButton1()
+{
+	UpdateData(TRUE);
+	HMODULE hServerDll = GetModuleHandle(L"server.dll"); // 获取模块基址
+	DWORD dwTempAddress;
+	SIZE_T dwRetNum;
+	ReadProcessMemory(GetCurrentProcess(), (LPCVOID)((DWORD)hServerDll + 0x4F518C), &dwTempAddress, 4, &dwRetNum);
+	BOOL bFlag = WriteProcessMemory(GetCurrentProcess(), (LPVOID)(dwTempAddress + 0x4B0), &m_Value, 4, &dwRetNum);
+	if (bFlag)
+	{
+		AfxMessageBox(L"修改完成！");
+	}
+	else
+	{
+		AfxMessageBox(L"修改失败！");
+	}
+	UpdateData(FALSE);
+}
---
+++ 简易修改器/MFCLibrary1/CSMFCDLL.h
@@ -0,0 +1,28 @@
+﻿#pragma once
+#include "afxdialogex.h"
+
+
+// CCSMFCDLL 对话框
+
+class CCSMFCDLL : public CDialogEx
+{
+	DECLARE_DYNAMIC(CCSMFCDLL)
+
+public:
+	CCSMFCDLL(CWnd* pParent = nullptr);   // 标准构造函数
+	virtual ~CCSMFCDLL();
+
+	// 对话框数据
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_DIALOG1 };
+#endif
+
+protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV 支持
+
+	DECLARE_MESSAGE_MAP()
+public:
+	// 子弹数量
+	DWORD m_Value;
+	afx_msg void OnBnClickedButton1();
+};
