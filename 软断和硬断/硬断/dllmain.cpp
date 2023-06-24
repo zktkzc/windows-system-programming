a9d04f9ed9340558d28da4c37c8c4f5ed2ac9cde
tkzc00 <zqzkzy2420586708@qq.com>
2023-06-24 19:44:39 +0800

    软断和硬断

---
+++ 软断和硬断/硬断/dllmain.cpp
@@ -0,0 +1,74 @@
+﻿// dllmain.cpp : 定义 DLL 应用程序的入口点。
+#include "pch.h"
+#include <Windows.h>
+#include <TlHelp32.h>
+
+size_t MessageBoxAddr = (size_t)GetProcAddress(GetModuleHandleA("User32.dll"), "MessageBoxA");
+
+LONG Handler(struct _EXCEPTION_POINTERS* ExceptionInfo)
+{
+	if ((ULONG_PTR)ExceptionInfo->ExceptionRecord->ExceptionAddress == MessageBoxAddr)
+	{
+		const char* szStr = "tkzc00";
+		*(DWORD*)(ExceptionInfo->ContextRecord->Esp + 0x8) = (DWORD)szStr;
+		ExceptionInfo->ContextRecord->Eip += 2;
+		return EXCEPTION_CONTINUE_EXECUTION;
+	}
+	else
+	{
+		ExceptionInfo->ContextRecord->Dr0 = MessageBoxAddr;
+		ExceptionInfo->ContextRecord->Dr7 = 0x405;
+		return EXCEPTION_CONTINUE_SEARCH;
+	}
+
+	return 0;
+}
+
+VOID SetThreadHook(HANDLE hThread)
+{
+	CONTEXT ctx;
+	ctx.ContextFlags = CONTEXT_ALL; // 获取线程上下文里的所有寄存器
+	GetThreadContext(hThread, &ctx);
+	ctx.Dr0 = MessageBoxAddr;
+	ctx.Dr7 = 0x1;
+	SetThreadContext(hThread, &ctx);
+}
+
+VOID SetHook()
+{
+	HANDLE hThreadShot = CreateToolhelp32Snapshot(TH32CS_SNAPTHREAD, GetCurrentProcessId());
+	THREADENTRY32* te32 = new THREADENTRY32;
+	te32->dwSize = sizeof(THREADENTRY32);
+	HANDLE hThread = NULL;
+	while (Thread32First(hThreadShot, te32))
+	{
+		if (GetCurrentProcessId() == te32->th32OwnerProcessID)
+		{
+			hThread = OpenThread(THREAD_ALL_ACCESS, FALSE, te32->th32ThreadID);
+			SetThreadHook(hThread);
+			CloseHandle(hThread);
+		}
+	}
+	CloseHandle(hThread);
+}
+
+BOOL APIENTRY DllMain(HMODULE hModule,
+	DWORD  ul_reason_for_call,
+	LPVOID lpReserved
+)
+{
+	switch (ul_reason_for_call)
+	{
+	case DLL_PROCESS_ATTACH:
+		AddVectoredExceptionHandler(1, (PVECTORED_EXCEPTION_HANDLER)Handler);
+		SetHook();
+	case DLL_THREAD_ATTACH:
+		SetThreadHook(GetCurrentThread());
+		break;
+	case DLL_THREAD_DETACH:
+	case DLL_PROCESS_DETACH:
+		break;
+	}
+	return TRUE;
+}
+
---
+++ 软断和硬断/软断/dllmain.cpp
@@ -0,0 +1,54 @@
+﻿// dllmain.cpp : 定义 DLL 应用程序的入口点。
+#include "pch.h"
+#include <Windows.h>
+
+size_t MessageBoxAddr = (size_t)GetProcAddress(GetModuleHandleA("User32.dll"), "MessageBoxA");
+
+ULONG_PTR ulpExceptionAddress = 0;
+UCHAR ucOldCode = 0;
+
+LONG Handler(struct _EXCEPTION_POINTERS* ExceptionInfo)
+{
+	if (ExceptionInfo->ExceptionRecord->ExceptionCode == EXCEPTION_BREAKPOINT)
+	{
+		if ((ULONG_PTR)ExceptionInfo->ExceptionRecord->ExceptionAddress == ulpExceptionAddress)
+		{
+			const char* szStr = "tkzc00";
+			*(DWORD*)(ExceptionInfo->ContextRecord->Esp + 0x8) = (DWORD)szStr;
+			ExceptionInfo->ContextRecord->Eip += 2;
+			return EXCEPTION_CONTINUE_EXECUTION;
+		}
+		return EXCEPTION_CONTINUE_SEARCH;
+	}
+
+	return 0;
+}
+
+VOID SetHook(ULONG_PTR ulpAddress)
+{
+	DWORD dwOldProtect = 0;
+	VirtualProtect((LPVOID)ulpAddress, 1, PAGE_EXECUTE_READWRITE, &dwOldProtect);
+	ulpExceptionAddress = ulpAddress;
+	ucOldCode = *(UCHAR*)ulpAddress;
+	*(UCHAR*)ulpAddress = 0xCC;
+	VirtualProtect((LPVOID)ulpAddress, 1, dwOldProtect, &dwOldProtect);
+}
+
+BOOL APIENTRY DllMain(HMODULE hModule,
+	DWORD  ul_reason_for_call,
+	LPVOID lpReserved
+)
+{
+	switch (ul_reason_for_call)
+	{
+	case DLL_PROCESS_ATTACH:
+		AddVectoredExceptionHandler(1, (PVECTORED_EXCEPTION_HANDLER)Handler);
+		SetHook(MessageBoxAddr);
+	case DLL_THREAD_ATTACH:
+	case DLL_THREAD_DETACH:
+	case DLL_PROCESS_DETACH:
+		break;
+	}
+	return TRUE;
+}
+
